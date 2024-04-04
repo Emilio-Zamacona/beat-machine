8e6f0c5ecf34404359e71850f6168952a0319960
emilio.zamacona <emiliozamacona@gmail.com>
2024-04-04 00:26:24 -0300

    fix square event

--- src/components/Grid/GridRow/GridSquare/GridSquare.tsx
+++ src/components/Grid/GridRow/GridSquare/GridSquare.tsx
@@ -1,7 +1,7 @@
-import { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { useStore } from '../../../../store/hooks';
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useStore } from "../../../../store/hooks";
 
 interface IGridSquare {
   square: number;
@@ -10,7 +10,12 @@ interface IGridSquare {
   onUpdateSquare: () => void;
 }
 
-const GridSquare = ({ square, rowIndex, squareIndex, onUpdateSquare }: IGridSquare) => {
+const GridSquare = ({
+  square,
+  rowIndex,
+  squareIndex,
+  onUpdateSquare,
+}: IGridSquare) => {
   const squareRef = useRef<HTMLButtonElement>(null);
   const { store } = useStore();
   const { theme, current } = store;
@@ -22,31 +27,30 @@ const GridSquare = ({ square, rowIndex, squareIndex, onUpdateSquare }: IGridSqua
       if (square && isCurrent()) {
         const tl = gsap.timeline().to(squareRef.current, {
           scale: 1.4,
-          backgroundColor: theme.sounds[rowIndex].color || theme.colors[0].value,
-          borderRadius: '50%',
-          boxShadow: '0px 0px 10px black',
+          backgroundColor:
+            theme.sounds[rowIndex].color || theme.colors[0].value,
+          borderRadius: "50%",
+          boxShadow: "0px 0px 10px black",
           duration: 0.3,
           onComplete: () => {
             tl.reverse();
           },
           onReverseComplete: () => {
-            gsap.set(squareRef.current, { clearProps: 'all' });
+            gsap.set(squareRef.current, { clearProps: "all" });
           },
         });
       }
     },
     { scope: squareRef, dependencies: [square, isCurrent()] }
   );
-  const onSquareClick = useGSAP(() => {
-    console.log('square click test');
-  });
   return (
     <button
       ref={squareRef}
-      className={`grid__row__square ${square && '--filled'} ${isCurrent() && '--current'}`}
+      className={`grid__row__square ${square && "--filled"} ${
+        isCurrent() && "--current"
+      }`}
       onMouseDown={() => {
-        onUpdateSquare;
-        onSquareClick;
+        onUpdateSquare();
       }}
     ></button>
   );
